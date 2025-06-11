a5691374558c13d090244f23d5907d9604e7ba09
Gage Nieffenegger <98041194+GAGEUALR@users.noreply.github.com>
2025-06-10 22:26:23 -0500

    testing...

--- RPGv1/main.c
+++ RPGv1/main.c
@@ -1,15 +1,28 @@
 #include "raylib.h"
 
 int main(void) {
-    InitWindow(1280, 720, "Raylib Verification Test");
+    InitWindow(1280, 720, "Test...");
+
+    Texture2D elder = LoadTexture("assets/sprites/goblin.png");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Raylib is working!", 250, 280, 20, DARKGRAY);
+
+        if (elder.id == 0) {
+            DrawText("Failed to load texture!", 10, 10, 20, RED);
+        }
+        else {
+            DrawTexture(elder, 600, 300, WHITE);
+            DrawText("Drawing...", 10, 10, 20, GREEN);
+        }
+
+        DrawText(GetWorkingDirectory(), 10, 40, 20, BLUE);
+
         EndDrawing();
     }
 
-    CloseWindow();  //hello world
+    UnloadTexture(elder);
+    CloseWindow();
     return 0;
 }
